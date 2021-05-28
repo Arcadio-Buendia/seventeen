fcd7ed3da5ba8be0e0cd808946b7cb1105d65a68
pawanspj <72201859+pawanspj@users.noreply.github.com>
2021-05-28 11:06:40 +0530

    Update lot_of_input.cpp

--- while/lot_of_input.cpp
+++ while/lot_of_input.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
 using namespace std;
-int main(){
-	cout<<"Enter a few numbers: ";
-	int value=0,sum=0;
-	while(cin>>value){
+int main()
+{
+	cout<<"Enter math scores for your students, seperated by space:\n ";
+	float value=0.0; 
+	float sum=0.0;
+	float countInputs=0.0;
+	while(cin>>value)
+	{
+		
+	
 		sum+=value;
+		countInputs++;
+	
 	}
-	cout<<"The sum of entered values is: "<<sum<<endl;
-	return 0;
+    
+    float ave=sum/countInputs;
+    cout<<"The average of their marks is:"<<ave;
+    return 0;
 }	
