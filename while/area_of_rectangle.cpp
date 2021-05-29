8a148b40df988664302027ce376e396c9203ed76
pawanspj <72201859+pawanspj@users.noreply.github.com>
2021-05-29 07:31:02 +0530

    Create area_of_rectangle.cpp

---
+++ while/area_of_rectangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+    cout<<"Enter the length followed by the breadth of rectangle \n";
+    float length=0,breadth=0;
+    cin>>length>>breadth;
+
+    while(length<20)
+        {
+            cout<<"The rectangle must be bigger than 20 cm long \n";
+            cout<<"Please re-enter the length:";
+            cin>>length;
+        }
+    cout<<"Area of the rectange is: "<<length*breadth;
+    return 0;
+ }
